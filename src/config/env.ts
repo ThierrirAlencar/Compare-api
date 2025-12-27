78f7d94eda8945b4ed675db2e5a5113ff615f52e
ciringa <thierrird957468@gmail.com>
2025-12-27 13:30:15 -0300

    [#01]feat: api error handler filter

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,11 @@
 import "dotenv/config"
 import z from "zod";
 
-export const {DATABASE_URL,API_JWT_CONFIG, API_JWT_TIME,NODE_ENV} = z.object({
+export const {DATABASE_URL,API_JWT_CONFIG, API_JWT_TIME,NODE_ENV, PORT,API_HOST} = z.object({
     DATABASE_URL: z.string(),
     API_JWT_CONFIG: z.string(),
     API_JWT_TIME:z.coerce.string(),
-    NODE_ENV:z.enum(["DEV","DEPLOY"])
+    NODE_ENV:z.enum(["DEV","DEPLOY"]),
+    PORT:z.coerce.number().default(3000),
+    API_HOST:z.string().default("0.0.0.0")
 }).parse(process.env)
---
+++ src/infrastructure/filters/exception.filter.ts
@@ -0,0 +1,53 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { baseError } from '../utils/errors';
+import { Response  } from 'express';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+import { ZodError } from 'zod';
+import { error } from 'console';
+
+//Quando qualquer erro for disparado na aplicação
+@Catch(Error)
+export class exceptionFilter implements ExceptionFilter{
+    catch(exception: Error, host: ArgumentsHost) {
+        //Load HTTP BS
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        
+        //send exceptions
+        if(exception instanceof baseError){
+            response
+                .status(exception.http_status)
+                .send(exception)
+                
+        }else if(exception instanceof PrismaClientKnownRequestError){
+            response
+                .status(500)
+                .send({
+                    http_status:500,
+                    description:"Unfiltered prisma error",
+                    error:{
+                        name:exception.name,
+                        message:exception.message
+                        //does not send full information over concerns of sensitive leak
+                    }
+                })
+        }else if(exception instanceof ZodError){
+            response
+                .status(400)
+                .send({
+                    http_status:400,
+                    description:"zod validation error",
+                    error:exception
+                })
+        }else {
+            response
+                .status(500)
+                .send({
+                    http_status:500,
+                    description:"unknow or unpredicted error",
+                    error:exception
+                })
+        }
+    }
+}
