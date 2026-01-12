af53e65ea0552fe838f69783ba619fe9530e5b43
ciringa <thierrird957468@gmail.com>
2026-01-12 10:23:25 -0300

    [#]fix:env and documentation on triggers

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ export const _env = z.object({
     DATABASE_URL: z.string(),
     API_JWT_CONFIG: z.string(),
     API_JWT_TIME:z.coerce.string(),
-    NODE_ENV:z.enum(["DEV","DEPLOY"]),
+    NODE_ENV:z.enum(["DEV","DEPLOY"]).default("DEV"),
     PORT:z.coerce.number().default(3000),
     API_HOST:z.string().default("0.0.0.0"),
     ADMIN_EMAIL:z.string().email(),
@@ -14,4 +14,15 @@ export const _env = z.object({
     REDIS_PORT:z.coerce.number()
 }).parse(process.env)
 
-export const {DATABASE_URL,API_JWT_CONFIG, API_JWT_TIME,NODE_ENV, PORT,API_HOST,ADMIN_EMAIL,ADMIN_PASSWORD,REDIS_HOST,REDIS_PORT} = _env
+export const {
+    DATABASE_URL,
+    API_JWT_CONFIG, 
+    API_JWT_TIME,
+    NODE_ENV, 
+    PORT,
+    API_HOST,
+    ADMIN_EMAIL,
+    ADMIN_PASSWORD,
+    REDIS_HOST,
+    REDIS_PORT
+    } = _env
